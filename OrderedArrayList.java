04f6c226e5abf4960aef76ad4f4d893ef735a05c
unknown <>
2020-12-14 00:57:11 -0500

    Adds Method: adds

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,4 +8,24 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int size) {
     super(size);
   }
+
+  public boolean add(T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Index can not be null.");
+    } else {
+      int i;
+      for (i = 0; i < super.size() && element.compareTo(super.get(i)) > 0; i++) {
+      }
+      super.add(i,element);
+      return true;
+    }
+  }
+
+  public void add(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Index can not be null.");
+    } else {
+      add(element);
+    }
+  }
 }
