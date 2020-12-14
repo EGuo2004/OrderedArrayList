3e1b4f2cbb623d748a8689b73d4b59037785e49a
unknown <>
2020-12-14 00:11:52 -0500

    Adds Method: set

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
 
   public NoNullArrayList() {
@@ -7,11 +8,12 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(int size) {
     super(size);
   }
-  public void add(T element) {
+  public boolean add(T element) {
     if(element == null) {
       throw new IllegalArgumentException("Can not add a null");
     } else {
       super.add(element);
+      return true;
     }
   }
 
@@ -22,5 +24,14 @@ public class NoNullArrayList<T> extends ArrayList<T> {
       super.add(index , element);
     }
   }
+
+  public T set(int index, T element) {
+    if(element == null) {
+      throw new IllegalArgumentException("Can not add a null");
+    } else {
+      T x = super.get(index);
+      super.set(index , element);
+      return x;
+    }
   }
 }
